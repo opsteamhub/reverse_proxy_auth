7c699048b52f89f1e68306f8d6e5459c2f9fb53f
Bruno Luis Cardoso Novo <bruno@Brunos-MacBook-Pro.local>
2022-08-15 14:20:38 -0300

    Adding script to create keycloak database inside RDS Instance

---
+++ rds_terraform/create_keycloak_db.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+export RDS_HOSTANAME="keycloak.csq3a574pjti.us-east-1.rds.amazonaws.com"
+export RDS_PORT=5432
+export RDS_USERNAME="keycloak"
+
+
+
+if [[ $(which psql) ]]; then
+    echo -e "\npsql installed, continuing database creation...\n"
+    else
+    echo -e "\npsql not installed!!\n"
+    exit 1
+fi
+
+PGPASSWORD=${TF_VAR_db_password} \
+    psql -h ${RDS_HOSTANAME} \
+     -p ${RDS_PORT} \
+     -U ${RDS_USERNAME} \
+     -d postgres \
+     -c "CREATE DATABASE keycloak"
+    #  -c "\list"
+
+echo -e "\nListing Databases...\n"
+
+PGPASSWORD=${TF_VAR_db_password} \
+    psql -h ${RDS_HOSTANAME} \
+     -p ${RDS_PORT} \
+     -U ${RDS_USERNAME} \
+     -d postgres \
+     -c "\list"
